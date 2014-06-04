11e67cbe8071534f2f251a9e5533d9f0de2ee949
owelov <owelov@gmx.de>
2014-06-04 17:54:59 +0200

    zweites commit

---
+++ HelloWorld/src/HELLo/HalloWelt.java
@@ -0,0 +1,13 @@
+package HELLo;
+
+public class HalloWelt {
+	
+	
+	public static String text = "Hallo Welt";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(text);
+	}
+
+}
