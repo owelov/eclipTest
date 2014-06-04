cb53cfca6b63364dd7c3991063593993037ef0fc
owelov <owelov@gmx.de>
2014-06-04 18:25:00 +0200

    was wilst du Git?

--- HelloWorld/src/HELLo/HalloWelt.java
+++ HelloWorld/src/HELLo/HalloWelt.java
@@ -6,7 +6,12 @@ public class HalloWelt {
 	public static String text = "Hallo du scheiß Welt";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Gebe Mir einen Sinnlosen Text aus!
+		einenSinnlosenTextAusgeben();
+	}
+	
+	public static void einenSinnlosenTextAusgeben()
+	{
 		System.out.println(text);
 	}
 
